f49ec6c87b76eb23494fbd43c6c28c5c00fb418b
backrooms9000 <Geancaio926@gmail.com>
2026-08-28 21:47:38 -0300

    cARR

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+
+
+
+/* Aviso de cookie*/
+
+const cookieBanner = document.getElementById("cookie-banner")
+const cookieAccept = document.getElementById("cookie-accept")
+
+// Verifica se o usuario ja aceitou os cookies
+if (localStorage.getItem("cookiesAcepted") === "true") {
+    cookieBanner.styler.display= "none";
+}
+
+// Botao "acriatar"
+cookieAccept.addEventListener("click", function () {
+    localStorage.setItem("cookiesAcepted", "true");
+    cookieBanner.styler.display= "none";
+});
